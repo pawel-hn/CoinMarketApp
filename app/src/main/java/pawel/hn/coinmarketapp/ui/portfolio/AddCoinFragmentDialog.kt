77fbd049c47c72b0ab5524b94bb1bc3f80e3cc4a
Pawel <hnacik.p@gmail.com>
2021-02-23 11:08:52 +0100

    added real time search

--- app/src/main/java/pawel/hn/coinmarketapp/ui/portfolio/AddCoinFragmentDialog.kt
+++ app/src/main/java/pawel/hn/coinmarketapp/ui/portfolio/AddCoinFragmentDialog.kt
@@ -1,20 +1,14 @@
 package pawel.hn.coinmarketapp.ui.portfolio
 
 
-import android.app.Dialog
-import android.content.Context
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.AdapterView
 import android.widget.ArrayAdapter
-import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
-import androidx.core.view.get
 import androidx.fragment.app.DialogFragment
-
 import pawel.hn.coinmarketapp.R
 import pawel.hn.coinmarketapp.TAG
 import pawel.hn.coinmarketapp.databinding.FragmentDialogAddEditBinding
@@ -27,15 +21,13 @@ class AddCoinFragmentDialog() : DialogFragment() {
     private lateinit var builder: AlertDialog.Builder
     private var chosenCoin: String? = null
 
+
     private val list = arrayOf("aaa", "bbb", "ccc", "ddd")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        Log.d(TAG, "onCreate called")
         args = AddCoinFragmentDialogArgs.fromBundle(requireArguments())
-        Log.d(TAG, "onCreate dialog: ${args.coinsNamesArray?.size ?: "args are null"}")
     }
-
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
@@ -44,9 +36,7 @@ class AddCoinFragmentDialog() : DialogFragment() {
         binding = FragmentDialogAddEditBinding.inflate(inflater, container, false)
         dialog!!.window!!.setBackgroundDrawableResource(R.drawable.dialog_round_corners)
            searchSpinner()
-        val adapter = ArrayAdapter(
-            requireContext(),
-            android.R.layout.simple_spinner_dropdown_item,
+        val adapter = ArrayAdapter(requireContext(), android.R.layout.simple_spinner_dropdown_item,
             args.coinsNamesArray!!
         )
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
@@ -54,19 +44,14 @@ class AddCoinFragmentDialog() : DialogFragment() {
           //  spinnerDialog.adapter = adapter
             spinnerDialogSearch.adapter = adapter
             btnDialogCancel.setOnClickListener {
-                Log.d(TAG, "cancel called")
-                Toast.makeText(
-                    parentFragment?.requireContext(),
-                    "cancel clikked",
-                    Toast.LENGTH_SHORT
-                ).show()
                 dismiss()
             }
             btnDialogSave.setOnClickListener {
-                Log.d(TAG, "save called")
                 dismiss()
             }
         }
+
+        
 
         return binding.root
     }
