0bed39a9572a79b32f22630a0cb7b05741a18555
Pawel <hnacik.p@gmail.com>
2021-08-05 23:00:10 +0200

    -buildSrc and its files

---
+++ buildSrc/src/main/java/DependenciesVersions.kt
@@ -0,0 +1,18 @@
+object DependenciesVersions {
+
+    val kotlinVersion = "1.5.10"
+    val kotlinKtxVersion = "1.6.0"
+    val appCompatVersion = "1.3.1"
+    val googleMaterialVersion = "1.4.0"
+    val navigationComponentVersion = "2.3.3"
+    val hiltVersion = "2.33-beta"
+    val retrofitVersion = "2.9.0"
+    val roomVersion = "2.3.0"
+    val coroutinesVersion = "1.4.3"
+    val workManagerVersion = "2.5.0"
+    val glideVersion = "4.11.0"
+    val settingsVersion = "1.1.1"
+    val chartsVersion = "3.1.0"
+    val rssParserVersion = "3.1.4"
+    val shimmmerVersion = ""
+}
